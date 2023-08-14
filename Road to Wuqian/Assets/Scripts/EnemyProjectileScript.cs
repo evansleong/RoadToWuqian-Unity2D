fc3da2944365adf2f634399196152514fb32adb9
lxy0626 <xylai224@gmail.com>
2023-08-14 12:19:45 +0800

    added stuff
    bird update
    background update
    more map object and abit more map

---
+++ Road to Wuqian/Assets/Scripts/EnemyProjectileScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+    public int damage;
+    public playerLife playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<playerLife>();
+
+        //set bullet to aim player
+        Vector2 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        //let bullet rotate
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 7)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Projectile Despawned");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerHealth.TakeDamage(damage);
+            Debug.Log("player health -2");
+            Destroy(this.gameObject);
+            Debug.Log("Projectile hit player");
+        }
+    }
+
+}
