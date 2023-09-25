095c0c744d54a6e53995f6aa1720672df3ae8204
lxy0626 <xylai224@gmail.com>
2023-09-25 12:14:03 +0800

    25/9/2023
    death scene
    menu script changes
    restart level need wip

--- Road to Wuqian/Assets/Scripts/playerLife.cs
+++ Road to Wuqian/Assets/Scripts/playerLife.cs
@@ -9,7 +9,9 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
     public int health;
     public HealthBar healthBar;
     [SerializeField] private AudioClip deathSound;
+    [SerializeField] private GameObject deathscene;
     private bool hasDeathSound = false;
+    private bool isDead = false;
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -32,10 +34,23 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
 
     private void Update()
     {
+        if (health != 0)
+        {
+            isDead = false;
+        }
         if (health <= 0 && !hasDeathSound)
         {
             Die();
+            if(isDead = true)
+            {
+                StartCoroutine(Deadscene());
+            }
+
         }
+        //else
+        //{
+        //    deathscene.SetActive(false);
+        //}
     }
 
     public void PlayHurtAnimation()
@@ -52,17 +67,17 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //if (collision.gameObject.CompareTag("Traps"))
-        //{
-        //    Die();
-        //}
-        //if (collision.gameObject.CompareTag("Checkpoint"))
-        //{
-        //    lastCheckPointPos = other.transform.position;
-        //}
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Traps"))
+    //    {
+    //        Die();
+    //    }
+    //    if (collision.gameObject.CompareTag("Checkpoint"))
+    //    {
+    //        lastCheckPointPos = other.transform.position;
+    //    }
+    //}
 
     //private void OnTriggerEnter2D(Collider2D other)
     //{
@@ -75,6 +90,7 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
     private void Die()
     {
         anim.SetTrigger("death");
+        isDead = true;
         rb.bodyType = RigidbodyType2D.Static;
         //Destroy(gameObject, 10);
         health = 0;
@@ -85,6 +101,7 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
             hasDeathSound = true;
         }
         StartCoroutine(RestartLevel());
+
     }
 
     IEnumerator RestartLevel()
@@ -102,5 +119,11 @@ public class playerLife : MonoBehaviour/*, IDataPersistance*/
     //{
     //    data.playerPos = this.transform.position;
     //}
+
+    IEnumerator Deadscene()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
 }
