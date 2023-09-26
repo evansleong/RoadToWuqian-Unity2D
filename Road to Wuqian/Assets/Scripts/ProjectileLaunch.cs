7c048c03fe85a7fd22e695ebd9881131f46bfe5b
Yihao24 <145582306+yihao0824@users.noreply.github.com>
2023-09-26 18:41:15 +0800

    1

--- Road to Wuqian/Assets/Scripts/ProjectileLaunch.cs
+++ Road to Wuqian/Assets/Scripts/ProjectileLaunch.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ProjectileLaunch : MonoBehaviour
 {
@@ -9,34 +11,101 @@ public class ProjectileLaunch : MonoBehaviour
     public Animator anim;
     [SerializeField] private AudioClip launchSound;
 
-    public float shootCooldown = 170.0f; // Cooldown time in seconds
+    public float shootCooldown = 5.0f; // Cooldown time in seconds
     private float cooldownTimer = 0.0f; // Timer for the cooldown
+    private bool isCooldown = false;
     private int remainingShots = 3;
     public float shootCounter;
+
+    [SerializeField] private Image imageCooldown;
+    [SerializeField] private TMP_Text textCooldown;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        textCooldown.gameObject.SetActive(false);
+        imageCooldown.fillAmount = 0.0f;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Reduce the cooldown timer if it's greater than zero
-        if (cooldownTimer > 0)
+        if(Input.GetKeyDown(KeyCode.Space) && remainingShots > 0 && cooldownTimer <= 0)
         {
-            cooldownTimer -= Time.deltaTime;
+            useFireball();
+        }
+        if(isCooldown)
+        {
+            ApplyCoolDown();
+        }
+    }
+
+    void ApplyCoolDown()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        if(remainingShots == 0)
+        {
+            isCooldown = false;
+            textCooldown.gameObject.SetActive(false);
+            imageCooldown.fillAmount = 0.0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && remainingShots > 0 && cooldownTimer <= 0)
+        if(cooldownTimer < 0.0f)
+        {
+            isCooldown = false;
+            textCooldown.gameObject.SetActive(false);
+            imageCooldown.fillAmount = 0.0f;
+        }
+        else
+        {
+            textCooldown.text = Mathf.RoundToInt(cooldownTimer).ToString();
+            imageCooldown.fillAmount = cooldownTimer / shootCooldown;
+        }
+    }
+
+    public bool useFireball()
+    {
+        if(isCooldown)
+        {
+            return false;
+        }
+        else
         {
             anim.SetTrigger("cast");
             SoundManager.instance.PlaySound(launchSound);
             Instantiate(projectilePrefab, launchPoint.position, Quaternion.identity);
             remainingShots--;
 
+            isCooldown = true;
+            textCooldown.gameObject.SetActive(true);
             cooldownTimer = shootCooldown;
+            return true;   
         }
     }
+
+    
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     // Reduce the cooldown timer if it's greater than zero
+    //     if (cooldownTimer > 0)
+    //     {
+    //         cooldownTimer -= Time.deltaTime;
+    //         isCooldown = true;
+    //     }
+
+    //     if (Input.GetKeyDown(KeyCode.Space) && remainingShots > 0 && cooldownTimer <= 0)
+    //     {
+    //         anim.SetTrigger("cast");
+    //         SoundManager.instance.PlaySound(launchSound);
+    //         Instantiate(projectilePrefab, launchPoint.position, Quaternion.identity);
+    //         remainingShots--;
+
+    //         isCooldown = false;
+    //         textCooldown.gameObject.SetActive(true);
+    //         cooldownTimer = shootCooldown;            
+    //     }
+    // }
 }
